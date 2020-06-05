5262c1b7c90212e2acddda1a7c2f80f3f5da1b07
maverick-finn <maverick.finn@g.austincc.edu>
2020-06-04 21:01:58 -0500

    Completed assignment2 for class, ready for grading

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -7,6 +7,20 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+/*
+Function will multiply num1 with function local variable num2
+
+param int num1
+
+*/
+
+int multiply_numbers(int num1)
+{
+	auto num2 = 5;
+	auto result = num1 * num2;
+
+	return result;
+}
 
 
 
