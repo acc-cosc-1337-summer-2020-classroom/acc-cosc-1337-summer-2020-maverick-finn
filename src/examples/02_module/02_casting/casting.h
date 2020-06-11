d761a91e00aed178fb296b9edb7abce02224da0c
maverick-finn <maverick.finn@g.austincc.edu>
2020-06-11 10:50:55 -0500

    Work from in class 6/9

--- src/examples/02_module/02_casting/casting.h
+++ src/examples/02_module/02_casting/casting.h
@@ -2,7 +2,10 @@
 Write double value-return function convert_to_double with an int years and
 double interest_rate parameter
 */
-
+double convert_int_to_double(int year, double interest_rate)
+{
+    return year * interest_rate;
+}
 
 /*
 Write int value-return function convert_double_to_int with a double parameter
--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,19 +1,43 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "expressions.h"
+
+
+int operator_precedence_1(int num1, int num2, int num3)
+{
+    return num1 + num2 / num3;
+}
+
+double convert_int_to_double(int year, double interest_rate)
+{
+    return year * interest_rate;
+}
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 /*Test case operator precedence 1 with function argument values 12, 6 and 3 returns 14*/
-
+TEST_CASE("Test operator precedence 1 with args: 12, 6 and 3 returns 14")
+{
+	REQUIRE(operator_precendence_1(12,6,3) == 14);
+}
 /*Test case operator precedence 2 with function argument values 12, 6 and 3 returns 6*/
-
+TEST_CASE("Test operator precedence 2 with args: 12, 6, and 3 returns 6")
+{
+	REQUIRE(operator_precedence_2(12,6,3) == 6);
+}
 /*Test case operator precedence 3 with function argument values 12, 6 and 3 returns 6*/
 
 /*Test case operator precedence 3 with function argument values 12, 6 and 3 returns 6*/
 
 /*Test case convert_to_double to show that multiplying int and double returns a double*/
+
+TEST_CASE("Test convert int to double implicity")
+{
+	REQUIRE(convert_int_to_double(10, .15) == 1.5);)
+}
 
 /*Test case convert double to int to show that a double converted to int truncates 
 the decimal portion*/
