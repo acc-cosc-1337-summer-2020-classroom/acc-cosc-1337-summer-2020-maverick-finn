fc6a5d03d68da9c7d51dcf9d104b2d6340aa78fb
maverick-finn <maverick.finn@g.austincc.edu>
2020-06-12 00:14:52 -0500

    Completed variables.cpp and .h files along with main for homework01, still need to write test cases

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,38 @@
 ﻿//write include statements
-
+#include<iostream>
+#include "variables.h"
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount{0}, tip_rate{0}, tip_amount{0}, tax_amount{0}, total{0};
+
+	cout<<"Enter meal amount: ";
+	cin>>meal_amount;
+	cout<<"\n";
+
+	tax_amount = get_sales_tax(meal_amount);
+
+	cout<<"Enter tip amount: ";
+	cin>>tip_rate;
+	cout<<"\n";
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+
+	total = meal_amount + tax_amount + tip_amount;
+
+	//printing receipt
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<<"Total: "<<total;
 
 	return 0;
 }
--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,5 +1,6 @@
 #include "variables.h"
 
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -9,6 +10,20 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+int multiply_numbers(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+double get_sales_tax(double meal_amount)
+{
+	double tax_rate = 0.0675
+
+	return meal_amount * tax_rate;
+}
 
 
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * tip_rate;
+}
