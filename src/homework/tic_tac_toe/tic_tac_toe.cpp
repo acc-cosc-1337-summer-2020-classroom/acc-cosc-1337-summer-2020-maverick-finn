c68bd9654560d2ec26a9fbe97cdf09ae17d997b0
maverick-finn <maverick.finn@g.austincc.edu>
2020-07-10 15:14:34 -0500

    Created and ran test cases. All test cases pass except for Test win by second row, and Test win by third row. Tried manually testing for a win by the second row, and it would work with certain entries of "O"s, and not work with others. Will have to get to the bottom of this issue.

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -54,6 +54,8 @@ void TicTacToe::display_board()const
     {
         std::cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
     }
+
+
 }
 
 
--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -31,6 +31,136 @@ TEST_CASE("Verify game ends when board is full")
 	tic_tac_toe.mark_board(8);
 	//true after all 9 slots are filled, even if out of order
 	REQUIRE(tic_tac_toe.game_over() == true);
+
+
+	REQUIRE(tic_tac_toe.get_winner() == "C");
 	
 }
+
+
+
+
+TEST_CASE("Test first player set to X")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+
+	REQUIRE(tic_tac_toe.get_player() == "X");
+}
+
+TEST_CASE("Test first player set to O")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("O");
+
+	REQUIRE(tic_tac_toe.get_player() == "O");
+}
+
+TEST_CASE("Test win by first column")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(1); //X
+	tic_tac_toe.mark_board(3); //O
+	tic_tac_toe.mark_board(4); //X
+	tic_tac_toe.mark_board(5); //O
+	tic_tac_toe.mark_board(7); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win by second column")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(2); //X
+	tic_tac_toe.mark_board(3); //O
+	tic_tac_toe.mark_board(5); //X
+	tic_tac_toe.mark_board(4); //O
+	tic_tac_toe.mark_board(8); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win by third column")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(3); //X
+	tic_tac_toe.mark_board(1); //O
+	tic_tac_toe.mark_board(6); //X
+	tic_tac_toe.mark_board(5); //O
+	tic_tac_toe.mark_board(9); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win by first row")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(1); //X
+	tic_tac_toe.mark_board(7); //O
+	tic_tac_toe.mark_board(2); //X
+	tic_tac_toe.mark_board(9); //O
+	tic_tac_toe.mark_board(3); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win by second row")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(4); //X
+	tic_tac_toe.mark_board(7); //O
+	tic_tac_toe.mark_board(5); //X
+	tic_tac_toe.mark_board(9); //O
+	tic_tac_toe.mark_board(6); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win by third row")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(7); //X
+	tic_tac_toe.mark_board(1); //O
+	tic_tac_toe.mark_board(8); //X
+	tic_tac_toe.mark_board(3); //O
+	tic_tac_toe.mark_board(9); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win diagonally from top left")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(1); //X
+	tic_tac_toe.mark_board(4); //O
+	tic_tac_toe.mark_board(5); //X
+	tic_tac_toe.mark_board(6); //O
+	tic_tac_toe.mark_board(9); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+TEST_CASE("Test win diagonally from bottomm left")
+{
+	TicTacToe tic_tac_toe;
+	tic_tac_toe.start_game("X");
+	tic_tac_toe.mark_board(7); //X
+	tic_tac_toe.mark_board(4); //O
+	tic_tac_toe.mark_board(5); //X
+	tic_tac_toe.mark_board(6); //O
+	tic_tac_toe.mark_board(3); //X, should win game
+
+	REQUIRE(tic_tac_toe.game_over() == true);
+}
+
+
+
+
 
