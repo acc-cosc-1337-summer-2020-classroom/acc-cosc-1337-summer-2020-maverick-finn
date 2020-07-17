c89cefde314fbc41e88863b9f5e16e16c0c3e07d
maverick-finn <maverick.finn@g.austincc.edu>
2020-07-17 18:56:47 -0500

    Wrote test cases and ran them. All pass. Anticipated to be last commit before turning in

--- test/classwork_test/10_assign_test/10_assign_tests.cpp
+++ test/classwork_test/10_assign_test/10_assign_tests.cpp
@@ -1,6 +1,31 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "employee.h"
+#include "engineer.h"
+#include "sales_employee.h"
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+
+
+TEST_CASE("Test Engineer class function get_pay")
+{
+	Employee *engineer = new Engineer(1500, 400);
+
+	double test_result = engineer -> get_pay();
+
+	REQUIRE(test_result == 1900);
+}
+
+
+TEST_CASE("Test SalesEmployee class function get_pay")
+{
+	Employee *sales_employee = new SalesEmployee(40, 10, 500);
+
+	double test_result = sales_employee -> get_pay();
+
+	REQUIRE(test_result == 900);
 }
